427b4c7f183537cfc1ddabb71eeec4bed7f5bc71
Stonetenth2005 <srivastavatushar2005@gmail.com>
2026-04-29 22:48:20 +0530

    Add Vercel configuration

--- server.js
+++ server.js
@@ -29,6 +29,11 @@ app.get('/api/health', (_req, res) => {
 
 // --------------- Start server ------------
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+}
+
+export default app;
